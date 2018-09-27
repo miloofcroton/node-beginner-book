bd5991f5a660f7eb993803b2d6555a00d24653a7
Jack <ajackofsometrades@gmail.com>
2018-09-27 13:12:01 -0700

    image uploading correctly!

--- server.js
+++ server.js
@@ -3,20 +3,9 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-        let postData = '';
         let pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} received`);
-        
-        request.setEncoding('utf8');
-
-        request.addListener('data', (postDataChunk) => {
-            postData += postDataChunk;
-            console.log(`received POST data chunk ${postDataChunk}.`);
-        });
-
-        request.addListener('end', () => {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
