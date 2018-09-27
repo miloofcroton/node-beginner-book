7896f9eef1df5c9cae97d694331fa729e7308a20
Jack <ajackofsometrades@gmail.com>
2018-09-27 11:32:17 -0700

    making request handlers work synchronously (bad)

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ function start(route, handle) {
     function onRequest(request, response) {
         const pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} received`);
-
-        route(handle, pathname);
         
         response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello world');
+        const content = route(handle, pathname);
+        response.write(content);
         response.end();
     }
 
